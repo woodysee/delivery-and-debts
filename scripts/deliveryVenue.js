51c0fffaabd3a5522eedab4a101b565de947e569
woodysee <woodyseesk@gmail.com>
2022-07-02 17:02:26 +0800

    Add random restaurant locations every game start

--- scripts/deliveryVenue.js
+++ scripts/deliveryVenue.js
@@ -1,28 +1,65 @@
 /* ALL CODE SHOULD GO BELOW HERE */
 
-var DeliveryVenue = function(){
-  this.render = function(interactions){
-  };
+var DeliveryVenue = function () {
+  this.render = function (interactions) {};
 };
 
-function createFirstDeliveryVenues(){
-  // create();
-  deliveryVenueElementA = document.getElementById('deliveryA');
-  deliveryVenueElementA.style.height = '60px';
-  deliveryVenueElementA.style.width = '40px';
-  deliveryVenueElementA.style.top = '10px';
-  deliveryVenueElementA.style.left = '150px';
+var deliveryVenueRows = Array.from({ length: 3 }).map((_, i) => {
+  return `${i * 120 + 20}px`;
+});
+var deliveryVenueCols = Array.from({ length: 4 }).map((_, i) => {
+  return `${i * 120 + 30}px`;
+});
+var deliveryVenuePossibilities = deliveryVenueRows.reduce(
+  (acc, row, rowIndex) => {
+    var workingAcc = Object.assign([], acc);
+    deliveryVenueCols.map((col, colIndex) => {
+      if (rowIndex !== 1 || colIndex !== 0) {
+        workingAcc.push([row, col]);
+      }
+    });
+    return workingAcc;
+  },
+  [],
+);
 
-  deliveryVenueElementB = document.getElementById('deliveryB');
-  deliveryVenueElementB.style.height = '60px';
-  deliveryVenueElementB.style.width = '40px';
-  deliveryVenueElementB.style.top = '140px';
-  deliveryVenueElementB.style.left = '270px';
+function createRandSequence() {
+  var workingDeliveryVenuePossibilities = Object.assign(
+    [],
+    deliveryVenuePossibilities,
+  );
+  return Array.from({ length: 3 }).reduce((acc, val) => {
+    var workingAcc = Object.assign([], acc);
+    var pseudorandomIndex = Math.floor(
+      Math.random() * workingDeliveryVenuePossibilities.length,
+    );
+    workingAcc.push(workingDeliveryVenuePossibilities[pseudorandomIndex]);
+    // Remove used location
+    workingDeliveryVenuePossibilities =
+      workingDeliveryVenuePossibilities.filter(
+        (_, i) => i !== pseudorandomIndex,
+      );
+    return workingAcc;
+  }, []);
+}
 
-  deliveryVenueElementC = document.getElementById('deliveryC');
-  deliveryVenueElementC.style.height = '60px';
-  deliveryVenueElementC.style.width = '40px';
-  deliveryVenueElementC.style.top = '270px';
-  deliveryVenueElementC.style.left = '390px';
+function createDeliveryVenues() {
+  var randomisedDeliveryVenues = createRandSequence();
+  deliveryVenueElementA = document.getElementById("deliveryA");
+  deliveryVenueElementA.style.height = "60px";
+  deliveryVenueElementA.style.width = "40px";
+  deliveryVenueElementA.style.top = randomisedDeliveryVenues[0][0];
+  deliveryVenueElementA.style.left = randomisedDeliveryVenues[0][1];
 
-};
+  deliveryVenueElementB = document.getElementById("deliveryB");
+  deliveryVenueElementB.style.height = "60px";
+  deliveryVenueElementB.style.width = "40px";
+  deliveryVenueElementB.style.top = randomisedDeliveryVenues[1][0];
+  deliveryVenueElementB.style.left = randomisedDeliveryVenues[1][1];
+
+  deliveryVenueElementC = document.getElementById("deliveryC");
+  deliveryVenueElementC.style.height = "60px";
+  deliveryVenueElementC.style.width = "40px";
+  deliveryVenueElementC.style.top = randomisedDeliveryVenues[2][0];
+  deliveryVenueElementC.style.left = randomisedDeliveryVenues[2][1];
+}
--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,4 @@
-console.log( "index.html linked to game.js." );
+console.log("index.html linked to game.js.");
 
 /* ALL CODE SHOULD GO BELOW HERE */
 
@@ -23,60 +23,58 @@ playerSpace.style.display = "none";
 
 //Win-lose conditions
 function winGame() {
-	gameState = "gameOff";
-	startPage.style.display = "none";
-	customPage.style.display = "none";
-	winPage.style.display = "block";
-	losePage.style.display = "none";
-	playerSpace.style.display = "none";
-};
+  gameState = "gameOff";
+  startPage.style.display = "none";
+  customPage.style.display = "none";
+  winPage.style.display = "block";
+  losePage.style.display = "none";
+  playerSpace.style.display = "none";
+}
 
 function loseGame() {
-	gameState = "gameOff";
-	startPage.style.display = "none";
-	customPage.style.display = "none";
-	winPage.style.display = "none";
-	losePage.style.display = "block";
-	playerSpace.style.display = "none";
-};
+  gameState = "gameOff";
+  startPage.style.display = "none";
+  customPage.style.display = "none";
+  winPage.style.display = "none";
+  losePage.style.display = "block";
+  playerSpace.style.display = "none";
+}
 
-var Game = function() {
+var Game = function () {
+  // Game settings
+  var settings = {}; // Contains all game settings
+  settings.mopedSpeed = 2; // The speed of the moped
+  settings.walls = true; // The moped can roam outside the world
+  settings.noclip = false; // The moped can pass through all world impassable objects
+  settings.minlunchCost = 10; // The price of delivering lunches to a single venue
+  settings.randomlunchRange = 50;
+  settings.initEarnings = 30; /*negative dollar(s)*/
+  settings.dayLength = 60; /*seconds*/ // Duration of a day (has to be between 0 - 600 seconds)
 
-	// Game settings
-	var settings = {};												// Contains all game settings
-	settings.mopedSpeed = 2;									// The speed of the moped
-	settings.walls = true;										// The moped can roam outside the world
-	settings.noclip = false;									// The moped can pass through all world impassable objects
-	settings.minlunchCost = 10;								// The price of delivering lunches to a single venue
-	settings.randomlunchRange = 50;
-	settings.initEarnings = 30;/*negative dollar(s)*/
-	settings.dayLength = 60; /*seconds*/			// Duration of a day (has to be between 0 - 600 seconds)
+  // Window settings
+  var assets = []; // All game objects
+  var player = new Moped(settings); // The player
+  assets[0] = player;
+  assets[1] = new Restaurant(); // The restaurant
+  assets[2] = new DeliveryVenue(); // The delivery venues
+  var frame = 0; // Frames since the start of the game
+  var secondsPassed = 0;
+  var gameIsPaused = false;
 
-	// Window settings
-	var assets = [];													// All game objects
-	var player = new Moped(settings);					// The player
-	assets[0] = player;
-	assets[1] = new Restaurant();							// The restaurant
-	assets[2] = new DeliveryVenue();					// The delivery venues
-	var frame = 0;														// Frames since the start of the game
-	var secondsPassed = 0;
-	var gameIsPaused = false;
+  // Interactions
+  var interactions = {};
+  interactions.up = false; // Up arrow key pressed
+  interactions.down = false; // Down arrow key pressed
+  interactions.left = false; // Left arrow key pressed
+  interactions.right = false; // Right arrow key pressed
 
-	// Interactions
-	var interactions = {};
-	interactions.up = false;									// Up arrow key pressed
-	interactions.down = false;								// Down arrow key pressed
-	interactions.left = false;								// Left arrow key pressed
-	interactions.right = false;								// Right arrow key pressed
-	
-	// Setup event listeners
-	function setupGameEvents() {
+  // Setup event listeners
+  function setupGameEvents() {
+    // Linking up, down, left and right keys to movement
+    document.addEventListener("keyup", function (event) {
+      var keyName = event.key;
 
-		// Linking up, down, left and right keys to movement
-		document.addEventListener('keyup', function(event){
-			var keyName = event.key;
-
-			switch (keyName) {
+      switch (keyName) {
         case "ArrowRight":
           interactions.right = false;
           break;
@@ -103,118 +101,129 @@ var Game = function() {
           break;
         default:
           break;
-      };
-		});
+      }
+    });
 
-		document.addEventListener('keydown', function(event){
-			var keyName = event.key;
-			// console.log(keyName);
-			switch (keyName) {
-				case "ArrowRight":
-					interactions.right = true;
-					break;
-				case "d":
-					interactions.right = true;
-					break;
-				case "ArrowLeft":
-					interactions.left = true;
-					break;
-				case "a":
-					interactions.left = true;
-					break;
-				case "ArrowUp":
-					interactions.up = true;
-					break;
-				case "w":
-					interactions.up = true;
-					break;
-				case "ArrowDown":
-					interactions.down = true;
-					break;
-				case "s":
-					interactions.down = true;
-					break;
-				default:
-					break;
-			};
-		});
+    document.addEventListener("keydown", function (event) {
+      var keyName = event.key;
+      // console.log(keyName);
+      switch (keyName) {
+        case "ArrowRight":
+          interactions.right = true;
+          break;
+        case "d":
+          interactions.right = true;
+          break;
+        case "ArrowLeft":
+          interactions.left = true;
+          break;
+        case "a":
+          interactions.left = true;
+          break;
+        case "ArrowUp":
+          interactions.up = true;
+          break;
+        case "w":
+          interactions.up = true;
+          break;
+        case "ArrowDown":
+          interactions.down = true;
+          break;
+        case "s":
+          interactions.down = true;
+          break;
+        default:
+          break;
+      }
+    });
 
-		document.querySelector('#closegame-button').addEventListener('click', closeGame); //Close game button on top left (Bankruptcy)
+    document
+      .querySelector("#closegame-button")
+      .addEventListener("click", closeGame); //Close game button on top left (Bankruptcy)
 
-		//Add more event listeners here
+    //Add more event listeners here
+  }
 
-	};
+  // The render function. It will be called 60/sec
+  this.render = function () {
+    for (var i = 0; i < assets.length; i++) {
+      assets[i].render(interactions);
+      timer(settings.dayLength, secondsPassed); //See dayTimer.js
+    }
+    secondsPassed = frame / 60; //no. of seconds passed per frame
+    frame++;
+    //console.log(frame, secondsPassed); // - Checks render loop. Uncomment and see console to see log.
+  };
 
-	// The render function. It will be called 60/sec
-	this.render = function(){
-		for(var i = 0; i < assets.length; i++){
-			assets[i].render(interactions);
-			timer(settings.dayLength, secondsPassed); //See dayTimer.js
-		};
-		secondsPassed = frame / 60; //no. of seconds passed per frame
-		frame++;
-		//console.log(frame, secondsPassed); // - Checks render loop. Uncomment and see console to see log.
-	};
+  var Game = this;
 
-	var Game = this;
+  runGame = () => {
+    window.requestAnimFrame = (function () {
+      return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+          window.setTimeout(callback, 1000 / 60);
+        }
+      );
+    })();
 
-	runGame = () => {
+    (function animloop() {
+      requestAnimFrame(animloop);
+      Game.render();
+    })();
+  };
 
-		window.requestAnimFrame = (function() {
-			return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
-				window.setTimeout(callback, 1000 / 60);
-			};
-		})();
+  // Startup the game
+  function init() {
+    runGame();
+    createDeliveryVenues();
+    setupGameEvents();
+  }
 
-		(function animloop() {
-			requestAnimFrame(animloop);
-			Game.render();
-		})();
-
-	}
-
-		// Startup the game
-	function init(){
-		runGame();
-		createFirstDeliveryVenues();
-		setupGameEvents();
-	};
-
-	init();
-
+  init();
 };
 /*End Game*/
 
 //Navigate between win/lose/start faux pages before and after game
 function fauxNav() {
-	if (gameState = "gameOff") {
-		function backHome() {
-			location.reload();
-		};
-		document.querySelector('#start-button').addEventListener('click', customGame);
-		document.querySelector('#custom-page-button').addEventListener('click', startGame);
-		document.querySelector('#lose-retry-button').addEventListener('click', backHome);
-		document.querySelector('#win-retry-button').addEventListener('click', backHome);
-		function customGame() {
-			gameState = "gameOn";
-			startPage.style.display = "none";
-			customPage.style.display = "block";
-			winPage.style.display = "none";
-			losePage.style.display = "none";
-			playerSpace.style.display = "none";
-			//console.log("test start button");
-		};
-		function startGame() {
-			new Game();
-			gameState = "gameOn";
-			startPage.style.display = "none";
-			customPage.style.display = "none";
-			winPage.style.display = "none";
-			losePage.style.display = "none";
-			playerSpace.style.display = "block";
-			//console.log("test start button");
-		};
-	};
-};
+  if ((gameState = "gameOff")) {
+    function backHome() {
+      location.reload();
+    }
+    document
+      .querySelector("#start-button")
+      .addEventListener("click", customGame);
+    document
+      .querySelector("#custom-page-button")
+      .addEventListener("click", startGame);
+    document
+      .querySelector("#lose-retry-button")
+      .addEventListener("click", backHome);
+    document
+      .querySelector("#win-retry-button")
+      .addEventListener("click", backHome);
+    function customGame() {
+      gameState = "gameOn";
+      startPage.style.display = "none";
+      customPage.style.display = "block";
+      winPage.style.display = "none";
+      losePage.style.display = "none";
+      playerSpace.style.display = "none";
+      //console.log("test start button");
+    }
+    function startGame() {
+      new Game();
+      gameState = "gameOn";
+      startPage.style.display = "none";
+      customPage.style.display = "none";
+      winPage.style.display = "none";
+      losePage.style.display = "none";
+      playerSpace.style.display = "block";
+      //console.log("test start button");
+    }
+  }
+}
 
 fauxNav();
